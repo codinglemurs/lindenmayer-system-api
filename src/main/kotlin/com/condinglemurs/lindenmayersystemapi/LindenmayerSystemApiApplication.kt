82391dbff7cdda069c1f7b75e27c41ba8c20628d
Michael da Costa <michaelcosta_11_17@hotmail.com>
2019-05-13 20:11:19 -0400

    Created an empty project.

---
+++ src/main/kotlin/com/condinglemurs/lindenmayersystemapi/LindenmayerSystemApiApplication.kt
@@ -0,0 +1,11 @@
+package com.condinglemurs.lindenmayersystemapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LindenmayerSystemApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LindenmayerSystemApiApplication>(*args)
+}
