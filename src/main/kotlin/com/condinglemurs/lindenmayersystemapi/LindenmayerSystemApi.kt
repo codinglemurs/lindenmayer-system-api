e8de97bedb093de72583dfcc1e9ac89bc607c505
Michael da Costa <michaelcosta_11_17@hotmail.com>
2019-05-13 21:59:08 -0400

    Updated source file formatting.

--- src/main/kotlin/com/condinglemurs/lindenmayersystemapi/LindenmayerSystemApi.kt
+++ src/main/kotlin/com/condinglemurs/lindenmayersystemapi/LindenmayerSystemApi.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class LindenmayerSystemApi
 
 fun main(args: Array<String>) {
-	runApplication<LindenmayerSystemApi>(*args)
+  runApplication<LindenmayerSystemApi>(*args)
 }
