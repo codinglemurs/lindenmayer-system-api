7e4b0fb61dd502e2622c6dfb8129be5ae2ba7f02
Michael da Costa <michaelcosta_11_17@hotmail.com>
2019-05-13 21:55:07 -0400

    Updated project settings again.

--- src/main/kotlin/com/condinglemurs/lindenmayersystemapi/LindenmayerSystemApi.kt
+++ src/main/kotlin/com/condinglemurs/lindenmayersystemapi/LindenmayerSystemApi.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LindenmayerSystemApiApplication
+class LindenmayerSystemApi
 
 fun main(args: Array<String>) {
-	runApplication<LindenmayerSystemApiApplication>(*args)
+	runApplication<LindenmayerSystemApi>(*args)
 }
